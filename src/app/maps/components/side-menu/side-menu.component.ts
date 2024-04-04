47ddb7314b8535a4afa04b90b2481b845d97362f
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-04 18:13:55 -0500

    feat: add side-menu and .env file

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+  icon: string;
+  alt: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styles: ``
 })
 export class SideMenuComponent {
+
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name:'Full Screen', icon:'../../assets/bx-fullscreen.svg', alt:'Full Screen Icom' },
+    { route: '/maps/zoom-range', name:'Zoom Range', icon:'../../assets/bx-zoom-in.svg', alt:'Zoom Range Icom' },
+    { route: '/maps/markers', name:'Markers', icon:'../../assets/bxs-map.svg', alt:'Markers Icom' },
+    { route: '/maps/properties', name:'Houses', icon:'../../assets/bxs-home.svg', alt:'Home Icon' },
+  ];
 
 }
--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     component: MapsLayoutComponent,
     children:[
       { path: 'fullscreen', component: FullScreenPageComponent },
-      { path: 'zoom-rage', component: ZoomRangePageComponent },
+      { path: 'zoom-range', component: ZoomRangePageComponent },
       { path: 'markers', component: MarkersPageComponent },
       { path: 'properties', component: PropertiesPageComponent },
       { path: '**', redirectTo: 'fullscreen' },
